fa0793517969f6d1e75b63c6f84475e0595342d9
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-03-17 20:27:00 +0300

    add: frontend part added with adding `src/templates` dir and changing `main.py` and `src/weather_service/router.py`

--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from weather_service.router import router as router_weather
 
-app = FastAPI()
-
+app = FastAPI(title='Weather Buddy')
 
 app.include_router(router_weather)
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+async def get_home_page(request: Request):
+    return templates.TemplateResponse('home.html', context={"request": request})
+
+
+@app.get('/about', response_class=HTMLResponse)
+async def get_home_page(request: Request):
+    return templates.TemplateResponse('about.html', context={"request": request})
 
 
 if __name__ == "__main__":
--- src/weather_service/router.py
+++ src/weather_service/router.py
@@ -1,6 +1,8 @@
 import aiohttp
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from src.config import WEATHER_API_KEY
 
@@ -9,9 +11,16 @@ router = APIRouter(
     tags=['Weather-service']
 )
 
+templates = Jinja2Templates(directory='templates')
 
-@router.get('/{city}')
-async def get_city_weather(city: str) -> dict:
+
+@router.get('/search', response_class=HTMLResponse)
+async def get_page_weather_search(request: Request):
+    return templates.TemplateResponse('city_weather_search.html', context={"request": request})
+
+
+@router.get('/{city}', response_class=HTMLResponse)
+async def get_city_weather(request: Request, city: str):
     url = 'http://api.weatherapi.com/v1/current.json'
     params = {
         'key': WEATHER_API_KEY,
@@ -20,6 +29,11 @@ async def get_city_weather(city: str) -> dict:
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url, params=params) as response:
             data = await response.json()
-            return data
+            weather_data = {
+                'location': data['location']['name'],
+                'temperature': data['current']['temp_c'],
+                'weather_condition': data['current']['condition']['text']
+            }
+            return templates.TemplateResponse('city_weather_present.html', context={"request": request, "weather_data": weather_data})
 
 
