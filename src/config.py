15a08da2b905af440ef063f773e61f2bc66ce766
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-03-21 14:56:56 +0300

    add: postgresql added with migrations by alembic

--- src/config.py
+++ src/config.py
@@ -5,3 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
