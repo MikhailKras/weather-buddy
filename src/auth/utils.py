ef4a6bd941ffe791fed443a3c3c26ac6bfecadf1
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-01 21:28:52 +0300

    add: in `src/auth/utils.py` added function for user authentication

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -3,6 +3,8 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.models import user
+from src.auth.schemas import UserInDB, AuthUser
+from src.auth.security import verify_password
 from src.database import get_async_session
 
 
@@ -12,7 +14,10 @@ async def get_user_by_username(
 ):
     select_query = select(user).where(user.c.username == username)
     result = await session.execute(select_query)
-    return result.all()
+    test = []
+    for row in result:
+        test.append(row.items())
+    print(test)
 
 
 async def get_user_by_email(
@@ -22,3 +27,15 @@ async def get_user_by_email(
     select_query = select(user).where(user.c.email == email)
     result = await session.execute(select_query)
     return result.all()
+
+
+def authenticate_user(
+        auth_data: AuthUser,
+        session: AsyncSession = Depends(get_async_session)
+):
+    user_data = await get_user_by_username(auth_data.username, session=session)
+    if not user:
+        return False
+    if not verify_password(auth_data.password, user_data.hashed_password):
+        return False
+    return user
