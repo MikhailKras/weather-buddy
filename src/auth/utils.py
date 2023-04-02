2b6e698cca0170ea6890b5db5a3d8857a9643728
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-02 18:30:53 +0300

    fix: typo removed in user authentication function (user changed to user_data); get user function now return UserInDB object instead of a sqlalchemy row object

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -12,21 +12,22 @@ async def get_user_by_username(
         username: str,
         session: AsyncSession = Depends(get_async_session)
 ):
+    column_names = [column.name for column in user.columns]
     select_query = select(user).where(user.c.username == username)
     result = await session.execute(select_query)
-    test = []
-    for row in result:
-        test.append(row.items())
-    print(test)
+    user_dict = {column_name: value for column_name, value in zip(column_names, result.fetchone().tuple())}
+    return UserInDB(**user_dict)
 
 
 async def get_user_by_email(
         email: str,
         session: AsyncSession = Depends(get_async_session)
 ):
+    column_names = [column.name for column in user.columns]
     select_query = select(user).where(user.c.email == email)
     result = await session.execute(select_query)
-    return result.all()
+    user_dict = {column_name: value for column_name, value in zip(column_names, result.fetchone().tuple())}
+    return UserInDB(**user_dict)
 
 
 def authenticate_user(
@@ -34,8 +35,8 @@ def authenticate_user(
         session: AsyncSession = Depends(get_async_session)
 ):
     user_data = await get_user_by_username(auth_data.username, session=session)
-    if not user:
+    if not user_data:
         return False
     if not verify_password(auth_data.password, user_data.hashed_password):
         return False
-    return user
+    return user_data
