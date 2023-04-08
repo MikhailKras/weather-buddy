e65b8cf74cd6e0cc0bb0934fa08de09b86002386
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-08 17:27:52 +0300

    feat: created function for creating access token

---
+++ src/auth/jwt.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+from jose import jwt
+
+from src.config import SECRET_KEY, ALGORITHM
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=10)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- src/config.py
+++ src/config.py
@@ -11,3 +11,6 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
