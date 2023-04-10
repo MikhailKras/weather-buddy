0bcb3dc7b230a328bc97e13a491c02eef3f416d4
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-10 19:33:32 +0300

    feat: added endpoint for login for access token

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,12 +1,20 @@
+from datetime import timedelta
+
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.auth.jwt import create_access_token
 from src.auth.models import user
-from src.auth.schemas import UserCreate, UserResponse
+from src.auth.schemas import UserCreate, UserResponse, Token
 from src.auth.security import get_password_hash
-from src.auth.utils import get_user_by_username, get_user_by_email
+from src.auth.utils import get_user_by_username, get_user_by_email, authenticate_user
 from src.database import get_async_session
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 10
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
 
 router = APIRouter(
     prefix='/users',
@@ -42,3 +50,27 @@ async def register_user(
         'city': user_data.city,
     }
     return response
+
+
+@router.post('/token', response_model=Token)
+async def login_for_access_token(
+        form_data: OAuth2PasswordRequestForm = Depends(),
+        session: AsyncSession = Depends(get_async_session)
+):
+    user_data = await authenticate_user(form_data, session=session)
+    if not user_data:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={'sub': user_data.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
