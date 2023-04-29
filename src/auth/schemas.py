ba794d2e359bcba01783a8f1d3d860af51c75ced
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-29 22:00:16 +0300

    feat: in the validation of the city when creating a user, the method for searching for a city has been changed

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -34,10 +34,10 @@ class UserCreate(BaseModel):
     @validator('city')
     def valid_city(cls, city: str) -> str:
         gc = geonamescache.GeonamesCache()
-        city_info = gc.get_cities_by_name(city.title())
+        city_info = gc.search_cities(city.title())
         if not city_info:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid city!')
-        return city.title()
+        return city_info[0]['name']
 
 
 class UserResponse(BaseModel):
