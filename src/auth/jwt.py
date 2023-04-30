78272da81f5ca2bb6c8845dcb4386dd206829e9d
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-04-30 14:25:23 +0300

    feat: get_current_user function added for routes protecting

--- src/auth/jwt.py
+++ src/auth/jwt.py
@@ -1,8 +1,16 @@
 from datetime import datetime, timedelta
 
-from jose import jwt
+from fastapi import status, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.auth.schemas import TokenData
+from src.auth.utils import get_user_by_username
 from src.config import SECRET_KEY, ALGORITHM
+from src.database import get_async_session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -14,3 +22,26 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        session: AsyncSession = Depends(get_async_session)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = await get_user_by_username(token_data.username, session=session)
+    if user is None:
+        raise credentials_exception
+    return user
