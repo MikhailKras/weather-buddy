ea01d224f193354686771aaefb3db13a129fc285
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-05-12 15:45:24 +0300

    feat: added tab menu for weather present, first tab - weather data, second tab - location data; added tab menu for weather search, first tab - search by city, second tab - search by coordinates (latitude, longitude)

--- src/static/js/weather_service/search.js
+++ src/static/js/weather_service/search.js
@@ -1,8 +1,8 @@
-function searchForOperation(event) {
+function searchByCity(event) {
   event.preventDefault();
   const cityInput = document.getElementById("city").value;
   const errorMessage = document.getElementById("error-message");
-  
+
   errorMessage.style.display = "none";
 
   fetch(`/weather/city_names?city_input=${cityInput}`)
@@ -13,6 +13,30 @@ function searchForOperation(event) {
         });
       }
       window.location.href = `/weather/city_names?city_input=${encodeURIComponent(cityInput)}`;
+    })
+    .catch((error) => {
+      errorMessage.textContent = error.message;
+      errorMessage.style.display = "block";
+    });
+}
+
+function searchByCoords(event) {
+  event.preventDefault();
+  const latitudeInput = document.getElementById("latitude").value;
+  const longitudeInput = document.getElementById("longitude").value;
+  const errorMessage = document.getElementById("error-message-coords");
+
+  errorMessage.style.display = "none";
+
+  redirect_url = "/weather/info?latitude=" + encodeURIComponent(latitudeInput) + "&longitude=" + encodeURIComponent(longitudeInput);
+  fetch(redirect_url)
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((data) => {
+          throw new Error(data.detail);
+        });
+      }
+      window.location.href = redirect_url;
     })
     .catch((error) => {
       errorMessage.textContent = error.message;
--- src/weather_service/router.py
+++ src/weather_service/router.py
@@ -21,7 +21,7 @@ async def get_page_weather_search(request: Request, is_auth: bool = Depends(is_a
     return templates.TemplateResponse('city_weather_search.html', context={"request": request, "is_auth": is_auth})
 
 
-@router.get('/city_names')
+@router.get('/city_names', response_class=HTMLResponse)
 async def find_city_name_matches(request: Request, city_input: str, is_auth: bool = Depends(is_authenticated)):
     gc = geonamescache.GeonamesCache()
     city_info = gc.search_cities(city_input.title())
@@ -31,32 +31,43 @@ async def find_city_name_matches(request: Request, city_input: str, is_auth: boo
     country_codes = [city_info[x]['countrycode'] for x in range(len(city_info))]
     countries_info: dict = gc.get_countries()
     country_names = [countries_info.get(country_code)['name'] for country_code in country_codes]
-    data = {'cities': [{'name': name, 'country': country} for name, country in zip(city_names, country_names)]}
+    coordinates = [{'latitude': city_info[x]['latitude'], 'longitude': city_info[x]['longitude']} for x in range(len(city_info))]
+    data = {'cities': [{'name': name, 'country': country, 'coordinates': coords} for name, country, coords in zip(city_names, country_names, coordinates)]}
     return templates.TemplateResponse(
         'city_names.html', context={"request": request, "data": data, "is_auth": is_auth}
     )
 
 
 @router.get('/info', response_class=HTMLResponse)
-async def get_city_weather(request: Request, city: str, country: str, is_auth: bool = Depends(is_authenticated)):
+async def get_city_weather(request: Request, latitude: float, longitude: float, is_auth: bool = Depends(is_authenticated)):
     url = 'http://api.weatherapi.com/v1/current.json'
     params = {
         'key': WEATHER_API_KEY,
-        'q': f"{city},{country}",
+        'q': f"{latitude},{longitude}",
     }
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url, params=params) as response:
             data = await response.json()
+            if 'error' in data:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=data['error']['message'])
             location_data = {
                 'location': data['location']['name'],
+                'region': data['location']['region'],
                 'country': data['location']['country'],
+                'latitude': data['location']['lat'],
+                'longitude': data['location']['lon'],
+                'timezone': data['location']['tz_id'],
+                'local time': data['location']['localtime']
             }
             weather_data = {
                 'temperature, °C': data['current']['temp_c'],
+                'feels like, °C': data['current']['feelslike_c'],
                 'weather condition': data['current']['condition']['text'],
                 'last updated': data['current']['last_updated'],
                 'wind, kph': data['current']['wind_kph'],
-                'humidity, %': data['current']['humidity']
+                'humidity, %': data['current']['humidity'],
+                'cloudiness, %': data['current']['cloud'],
+
             }
 
             return templates.TemplateResponse(
