24c934c6a40d5eb3989eabe4a854bc0539905e73
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-05-16 20:26:19 +0300

    feat: added new db table `email_verification` for email verification process during registration

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, Boolean, TIMESTAMP, Float, CheckConstraint
+from sqlalchemy import MetaData, Table, Column, Integer, String, Boolean, TIMESTAMP, Float, CheckConstraint, ForeignKey
 
 metadata = MetaData()
 
@@ -17,4 +17,14 @@ user = Table(
     Column('registered_at', TIMESTAMP, default=datetime.utcnow),
     Column('latitude', Float, CheckConstraint('latitude >= -90.0 AND latitude <= 90.0')),
     Column('longitude', Float, CheckConstraint('longitude >= -180.0 AND longitude <= 180.0'))
+)
+
+email_verification = Table(
+    'email_verification',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', Integer, ForeignKey('user.id'), nullable=False),
+    Column('token', String, nullable=False),
+    Column('verified', Boolean, nullable=False, default=False),
+    Column('created_at', TIMESTAMP, default=datetime.utcnow)
 )
