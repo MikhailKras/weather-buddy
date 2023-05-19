5d6dcb69d729109da245fe9d3b89d9fa32848b73
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-05-19 22:44:36 +0300

    test: added all remaining unit tests for `auth/router.py` and fix minor issues in existing tests

--- tests/auth/test_router.py
+++ tests/auth/test_router.py
@@ -1,20 +1,24 @@
 import pytest
 from httpx import AsyncClient
 
+import src
 from src.auth.email import Email
 from src.auth.jwt import is_authenticated
 from src.main import app
 
 
-async def test_register_step_1_auth(ac: AsyncClient, monkeypatch):
+async def test_register_step_1_auth(ac: AsyncClient):
     def override_is_authenticated():
         return True
+
     app.dependency_overrides[is_authenticated] = override_is_authenticated
 
     response = await ac.get("/users/register/city")
 
     assert response.status_code == 307
     assert response.headers["location"] == "/users/me"
+
+    app.dependency_overrides.pop(is_authenticated)
 
 
 async def test_register_step_1_not_auth(ac: AsyncClient):
@@ -87,10 +91,13 @@ async def test_register_step_2(
     if location:
         assert response.headers["location"] == location
 
+    ac.cookies.delete("registration_token")
 
-async def test_register_step_2_submit(ac: AsyncClient, monkeypatch):
-    async def send_verification_code_mock(self):
+
+async def test_register_step_2_submit(ac: AsyncClient, monkeypatch: pytest.MonkeyPatch):
+    async def send_verification_code_mock(*args, **kwargs):
         pass
+
     monkeypatch.setattr(Email, "send_verification_code", send_verification_code_mock)
 
     response = await ac.post("/users/register/details", json={
@@ -104,7 +111,6 @@ async def test_register_step_2_submit(ac: AsyncClient, monkeypatch):
 
 
 async def test_register_step_2_submit_existing_username(ac: AsyncClient, existing_user):
-
     response = await ac.post("/users/register/details", json={
         "username": existing_user["username"],
         "password": "string1",
@@ -116,7 +122,6 @@ async def test_register_step_2_submit_existing_username(ac: AsyncClient, existin
 
 
 async def test_register_step_2_submit_existing_email(ac: AsyncClient, existing_user):
-
     response = await ac.post("/users/register/details", json={
         "username": "test_user2",
         "password": "string1",
@@ -125,3 +130,290 @@ async def test_register_step_2_submit_existing_email(ac: AsyncClient, existing_u
 
     assert response.status_code == 422
     assert response.json()["detail"] == "This email is already registered!"
+
+
+async def test_register_success(ac: AsyncClient):
+    message = "Registration successful!"
+    response = await ac.get(f"/users/register/success?message={message}")
+
+    assert response.status_code == 200
+    assert "registration_token" in response.headers["set-cookie"]
+    assert 'text/html' in response.headers["content-type"]
+
+
+async def test_verify_email_page(ac: AsyncClient, registration_token):
+    response = await ac.get(f"/users/verify-email-page/{registration_token}")
+
+    assert 'text/html' in response.headers["content-type"]
+
+
+@pytest.mark.parametrize(
+    "token, expected_status, expected_detail, expected_message, user_data",
+    [
+        ("test_token_1", 200, None, "Email verification successful", "some user data"),
+        ("test_token_2", 400, "Email already verified", None, "some user data"),
+        ("test_token_3", 404, "User not found", None, None),
+        ("fake_token", 404, "Email verification token not found", None, "some user data"),
+    ]
+)
+async def test_verify_email(ac: AsyncClient,
+                            existing_verifications,
+                            token,
+                            expected_status,
+                            expected_detail,
+                            expected_message,
+                            user_data,
+                            monkeypatch: pytest.MonkeyPatch
+                            ):
+    def get_email_from_token_mock(*args, **kwargs):
+        return "email"
+
+    async def get_user_by_email_mock(*args, **kwargs):
+        return user_data
+
+    monkeypatch.setattr(src.auth.router, "get_email_from_token", get_email_from_token_mock)
+    monkeypatch.setattr(src.auth.router, "get_user_by_email", get_user_by_email_mock)
+    response = await ac.get(f"/users/verify-email/{token}")
+    assert response.status_code == expected_status
+    if expected_detail:
+        assert response.json()["detail"] == expected_detail
+    if expected_message:
+        assert response.json()["message"] == expected_message
+
+
+@pytest.mark.parametrize(
+    "is_auth, expected_status, location, content_type",
+    [
+        (True, 307, "/users/me", None),
+        (False, 200, None, "text/html")
+    ]
+)
+async def test_login_user_get_form(
+        ac: AsyncClient, is_auth,
+        expected_status,
+        location,
+        content_type
+):
+    def override_is_authenticated():
+        return is_auth
+
+    app.dependency_overrides[is_authenticated] = override_is_authenticated
+
+    response = await ac.get("/users/login")
+
+    assert response.status_code == expected_status
+
+    if location:
+        assert response.headers["location"] == location
+    if content_type:
+        assert content_type in response.headers["content-type"]
+
+    app.dependency_overrides.pop(is_authenticated)
+
+
+@pytest.mark.parametrize(
+    "form_data, expected_status, expected_detail",
+    [
+        (True, 200, None),
+        (None, 401, "Incorrect username or password")
+    ]
+)
+async def test_login_for_access_token(
+        ac: AsyncClient,
+        form_data,
+        expected_status,
+        expected_detail,
+        monkeypatch: pytest.MonkeyPatch
+):
+    async def authenticate_user_mock(*args, **kwargs):
+        if form_data:
+            class fake_form_data:
+                username = "test_user"
+
+            return fake_form_data
+        return None
+
+    fake_token_value = "some_token"
+
+    def create_access_token_mock(*args, **kwargs):
+        return fake_token_value
+
+    monkeypatch.setattr(src.auth.router, "authenticate_user", authenticate_user_mock)
+    monkeypatch.setattr(src.auth.router, "create_access_token", create_access_token_mock)
+
+    response = await ac.post("/users/token", data={
+        "username": "test_user",
+        "password": "string1"
+    })
+
+    assert response.status_code == expected_status
+
+    if expected_detail:
+        assert response.json()["detail"] == expected_detail
+
+    if form_data:
+        assert response.cookies["access_token"] == '"Bearer {}"'.format(fake_token_value)
+        assert response.json()["access_token"] == fake_token_value
+        assert response.json()["token_type"] == "bearer"
+
+
+async def test_read_users_me(ac: AsyncClient):
+    response = await ac.get("/users/me")
+
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+@pytest.mark.parametrize(
+    "is_auth, expected_status, location",
+    [
+        (True, 307, "/users/login"),
+        (False, 307, "/users/login")
+    ]
+)
+async def test_logout(
+        ac: AsyncClient,
+        is_auth,
+        expected_status,
+        location
+):
+    def override_is_authenticated():
+        return is_auth
+
+    app.dependency_overrides[is_authenticated] = override_is_authenticated
+
+    response = await ac.get("/users/logout")
+    if is_auth:
+        assert "access_token" in response.headers["set-cookie"]
+
+    assert response.status_code == expected_status
+    app.dependency_overrides.pop(is_authenticated)
+
+
+async def test_get_account_settings(ac: AsyncClient):
+    response = await ac.get("/users/settings")
+
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+
+@pytest.mark.parametrize(
+    "new_data, expected_status, detail, message",
+    [
+        ({"username": "new_username", "email": "new_email@mail.com"}, 200, None, "Data changed successfully!"),
+        ({"username": "test_user", "email": "new_email@mail.com"}, 422, "This username is already registered!", None),
+        ({"username": "new_username", "email": "test_user@gmail.com"}, 422, "This email is already registered!", None),
+        ({"username": "my_user", "email": "my_user@gmail.com"}, 400, "No changes detected", None)
+    ]
+)
+async def test_update_user_data(
+        ac: AsyncClient,
+        new_data,
+        expected_status,
+        detail,
+        message,
+        existing_user
+):
+    response = await ac.patch("/users/settings/update_data", json=new_data)
+    if detail:
+        assert response.json()["detail"] == detail
+    if message:
+        assert response.json()["message"] == message
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    "new_city_data, expected_status, detail, message",
+    [
+        ({"city": "Berlin", "country": "Germany", "latitude": 52.52, "longitude": 13.41}, 200, None, "City changed successfully!"),
+        ({"city": "Brussels", "country": "Belgium", "latitude": 52.52, "longitude": 13.41}, 400, "City already chosen", None),
+    ]
+)
+async def test_change_city_data(
+        ac: AsyncClient,
+        new_city_data,
+        expected_status,
+        detail,
+        message
+):
+    response = await ac.patch("/users/settings/change_city_data", json=new_city_data)
+
+    if detail:
+        assert response.json()["detail"] == detail
+    if message:
+        assert response.json()["message"] == message
+
+
+@pytest.mark.parametrize(
+    "passwords, password_in_db, expected_status, message, detail",
+    [
+        ({
+             "current_password": "correct_password1",
+             "repeat_password": "correct_password1",
+             "new_password": "new_password1"
+         }, "correct_password1", 200, "Password changed successfully!", None),
+        ({
+            "current_password": "correct_password1",
+            "repeat_password": "another_password1",
+            "new_password": "new_password1"
+        }, "correct_password1", 400, None, "The provided passwords do not match"),
+        ({
+            "current_password": "correct_password1",
+            "repeat_password": "correct_password1",
+            "new_password": "correct_password1"
+        }, "correct_password1", 400, None, "New password must be different from current password"),
+        ({
+            "current_password": "correct_password1",
+            "repeat_password": "correct_password1",
+            "new_password": "bad_pattern"
+        }, "correct_password1", 400, None, "New password must contain at least one digit, at least one letter"),
+        ({
+            "current_password": "correct_password1",
+            "repeat_password": "correct_password1",
+            "new_password": "short1"
+        }, "correct_password1", 400, None, "Length of new password must be more than 6 characters"),
+        ({
+            "current_password": "incorrect_password1",
+            "repeat_password": "incorrect_password1",
+            "new_password": "new_password1"
+        }, "correct_password1", 401, None, "Current password is incorrect")
+    ]
+)
+async def test_change_password(
+        ac: AsyncClient,
+        passwords,
+        password_in_db,
+        expected_status,
+        message,
+        detail,
+        monkeypatch: pytest.MonkeyPatch
+):
+    def verify_password_mock(*args, **kwargs):
+        return passwords["current_password"] == password_in_db
+
+    monkeypatch.setattr(src.auth.router, "verify_password", verify_password_mock)
+
+    response = await ac.patch("/users/settings/change_password", json=passwords)
+
+    assert response.status_code == expected_status
+
+    if detail:
+        assert response.json()["detail"] == detail
+
+    if message:
+        assert response.json()["message"] == message
+
+
+async def test_delete_user(ac: AsyncClient):
+    response = await ac.delete("/users/settings/delete_user")
+
+    assert response.status_code == 204
+    assert "access_token" in response.headers["set-cookie"]
+
+
+async def test_get_my_city_info(ac: AsyncClient, city_data):
+    response = await ac.get("/users/my_city_info")
+
+    assert response.status_code == 307
+    assert response.headers["location"] == \
+           f"/weather/info?latitude={city_data['latitude']}&longitude={city_data['longitude']}&city={city_data['city']}"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 from typing import AsyncGenerator
 
 import pytest
@@ -7,9 +8,9 @@ from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from src.auth.jwt import get_current_city_data, create_registration_token, is_authenticated
-from src.auth.models import user
-from src.auth.schemas import UserCreateStep1
+from src.auth.jwt import get_current_city_data, create_registration_token, get_current_user
+from src.auth.models import user, email_verification
+from src.auth.schemas import UserCreateStep1, UserInDB
 from src.config import DB_USER_TEST, DB_PASS_TEST, DB_HOST_TEST, DB_PORT_TEST, DB_NAME_TEST
 from src.database import metadata, get_async_session
 from src.main import app
@@ -89,8 +90,8 @@ app.dependency_overrides[get_current_city_data] = override_get_current_city_data
 async def existing_user():
     async with async_session_maker() as session:
         existing_user_data = {
-            "username": "existing_user",
-            "email": "existing_user@example.com"
+            "username": "test_user",
+            "email": "test_user@gmail.com"
         }
         insert_query = insert(user).values(
             username=existing_user_data["username"],
@@ -105,3 +106,68 @@ async def existing_user():
         await session.commit()
 
         yield existing_user_data
+
+
+@pytest.fixture
+async def existing_verifications():
+    async with async_session_maker() as session:
+        data = [
+            {
+                "username": "test_user_1",
+                "email": "test_user_1@test.test",
+                "user_id": 1,
+                "token": "test_token_1",
+                "verified": False
+            },
+            {
+                "username": "test_user_2",
+                "email": "test_user_2@test.test",
+                "user_id": 2,
+                "token": "test_token_2",
+                "verified": True
+            },
+            {
+                "username": "test_user_3",
+                "email": "test_user_3@test.test",
+                "user_id": 3,
+                "token": "test_token_3",
+                "verified": False
+            }
+        ]
+        for row in data:
+            insert_query_user = insert(user).values(
+                username=row["username"],
+                email=row["email"],
+                hashed_password="test",
+                city="test",
+                country="test",
+                latitude=0.0,
+                longitude=0.0
+            )
+            await session.execute(insert_query_user)
+
+            insert_query_verification = insert(email_verification).values(
+                user_id=row["user_id"],
+                token=row["token"],
+                verified=row["verified"]
+            )
+            await session.execute(insert_query_verification)
+        await session.commit()
+
+
+async def override_get_current_user():
+    return UserInDB(
+        id=1,
+        username="my_user",
+        email="my_user@gmail.com",
+        hashed_password="test",
+        registered_at=datetime.datetime.utcnow(),
+        disabled=False,
+        city="Brussels",
+        country="Belgium",
+        latitude=50.85045,
+        longitude=4.34878
+    )
+
+
+app.dependency_overrides[get_current_user] = override_get_current_user
