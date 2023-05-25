3effe59981613ad6ebb8a384d778bcc7a91a351b
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-05-25 06:43:16 +0300

    refactor: update alembic environment for model reorganization
    This commit updates the alembic environment to reflect changes in the organization of SQLAlchemy models.
    
    Changes Made:
    - Updated the imports in `alembic/env.py` to include the new model locations.
    - Imported `src.models` and `src.auth.models` to ensure all models are accessible.
    - Added `src.database.Base` and `src.database.metadata` to the target metadata for migration.
    - Updated the configuration for `target_metadata` to include both `src.auth.models.metadata` and `src.database.Base.metadata`.

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.auth.models import metadata
+from src.models import *
+from src.auth.models import *
 from src.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS
+from src.database import Base, metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -28,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [metadata, Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
