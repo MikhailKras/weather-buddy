a26da2f14d502c5e39652029c690dc77153d6ba6
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-06-22 03:31:33 +0300

    test: added load testing with `password_reset_request.lua` script for `post_email_for_password_reset` endpoint testing

---
+++ load_testing/password_reset_request.lua
@@ -0,0 +1,14 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.body = '{"email": "mikhailkrasovitskiy@gmail.com"}'
+
+local counter = 1
+
+function response(status, headers, body)
+   print("Response Body:", body)
+
+   if counter == 4 then
+      wrk.thread:stop()
+   end
+   counter = counter + 1
+end
