d3028dcc636fc69bdf061cf54c67dcad71b5493d
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-07-09 20:48:48 +0300

    feat: added loading spinner functionality to login form
    - Added CSS rule to hide the spinner by default
    - Added event listener to the submit button to show the spinner when clicked, after checking for valid input
    - Added check for valid input before making the fetch request in the submit form event listener

--- src/static/js/login.js
+++ src/static/js/login.js
@@ -1,12 +1,29 @@
 const loginForm = document.getElementById("login-form");
 const errorMessage = document.getElementById("error-message");
 const successMessage = document.getElementById("success-message");
+const spinner = document.querySelector(".spinner-border");
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", () => {
+  const username = loginForm.elements.username.value;
+  const password = loginForm.elements.password.value;
+
+  if (username.trim() === "" || password.trim() === "") {
+    return;
+  }
+
+  spinner.style.display = "inline-block";
+});
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const username = loginForm.elements.username.value;
   const password = loginForm.elements.password.value;
+
+  if (username.trim() === "" || password.trim() === "") {
+    return;
+  }
 
   errorMessage.style.display = "none";
 
@@ -40,5 +57,8 @@ loginForm.addEventListener("submit", (event) => {
     .catch((error) => {
       errorMessage.textContent = error.message;
       errorMessage.style.display = "block";
+    })
+    .finally(() => {
+      spinner.style.display = "none";
     });
 });
