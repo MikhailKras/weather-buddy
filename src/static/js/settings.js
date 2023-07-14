be9b972cebab0cd911cc447288e12ff930acc1cd
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-07-14 18:38:12 +0300

    feat: added loading spinners to settings

--- src/static/js/settings.js
+++ src/static/js/settings.js
@@ -20,6 +20,9 @@ document.addEventListener("DOMContentLoaded", () => {
 const changeDataForm = document.getElementById("change-data-form");
 const errorMessage = document.getElementById("error-message");
 const successMessage = document.getElementById("success-message");
+const spinner = document.querySelector(
+  "#submit-button-username-email .spinner-border"
+);
 
 changeDataForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -30,6 +33,7 @@ changeDataForm.addEventListener("submit", (event) => {
   };
 
   errorMessage.style.display = "none";
+  spinner.style.display = "inline-block";
 
   fetch("/users/settings/update_data", {
     method: "PATCH",
@@ -55,17 +59,23 @@ changeDataForm.addEventListener("submit", (event) => {
       successMessage.style.display = "none";
       errorMessage.textContent = error.message;
       errorMessage.style.display = "block";
+    })
+    .finally(() => {
+      spinner.style.display = "none";
     });
 });
 
 const changeCityForm = document.getElementById("change-city-data-form");
 const errorMessageCity = document.getElementById("error-message-city");
 const successMessageCity = document.getElementById("success-message-city");
-
+const spinner_city = document.querySelector(
+  "#submit-button-city .spinner-border"
+);
 changeCityForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cityInput = changeCityForm.elements.city.value;
+  spinner_city.style.display = "inline-block";
 
   fetch(
     `/users/settings/city/choose_city_name?city_input=${encodeURIComponent(
@@ -85,6 +95,9 @@ changeCityForm.addEventListener("submit", (event) => {
     .catch((error) => {
       errorMessageCity.textContent = error.message;
       errorMessageCity.style.display = "block";
+    })
+    .finally(() => {
+      spinner_city.style.display = "none";
     });
 });
 
@@ -92,6 +105,9 @@ const changePasswordForm = document.getElementById("change-password-form");
 const errorMessagePassword = document.getElementById("error-message-password");
 const successMessagePassword = document.getElementById(
   "success-message-password"
+);
+const spinner_password = document.querySelector(
+  "#submit-button-password .spinner-border"
 );
 
 changePasswordForm.addEventListener("submit", (event) => {
@@ -104,6 +120,7 @@ changePasswordForm.addEventListener("submit", (event) => {
   };
 
   errorMessagePassword.style.display = "none";
+  spinner_password.style.display = "inline-block";
 
   fetch("/users/settings/change_password", {
     method: "PATCH",
@@ -131,12 +148,18 @@ changePasswordForm.addEventListener("submit", (event) => {
     .catch((error) => {
       errorMessagePassword.textContent = error.message;
       errorMessagePassword.style.display = "block";
+    })
+    .finally(() => {
+      spinner_password.style.display = "none";
     });
 });
 
 const deleteBtn = document.getElementById("delete-btn");
 const confirmDeleteBtn = document.getElementById("confirm-delete-btn");
 const confirmationInput = document.getElementById("confirmation-input");
+const spinner_delete = document.querySelector(
+  "#confirm-delete-btn .spinner-border"
+);
 
 deleteBtn.addEventListener("click", () => {
   // Clear the confirmation input field when the modal is opened
@@ -150,6 +173,8 @@ confirmDeleteBtn.addEventListener("click", async () => {
     alert('Please enter the word "DELETE" to confirm the account deletion.');
     return;
   }
+
+  spinner_delete.style.display = "inline-block";
 
   const response = await fetch("/users/settings/delete_user", {
     method: "DELETE",
@@ -169,6 +194,8 @@ confirmDeleteBtn.addEventListener("click", async () => {
     // Handle error response
     console.error(`Error deleting account: ${response.statusText}`);
   }
+
+  spinner_delete.style.display = "none";
 });
 
 // Prevent form submission on Enter key press
