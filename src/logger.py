e72bf34b27dd0759430eae07193c239b12ad92dc
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-08-15 18:54:36 +0300

    feat: added rotating to logger by size

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,27 @@
-import logging
+import datetime
+import gzip
+import logging.handlers
+import shutil
+
 from pythonjsonlogger import jsonlogger
 
-logger = logging.getLogger(__name__)
 
-handler = logging.FileHandler('logs/request_logs.json')
+def namer(name):
+    now = datetime.datetime.now()
+    return f'{name}_{now.strftime("%d-%m-%y_%H-%M")}.gz'
+
+
+def rotator(source, dest):
+    with open(source, 'rb') as file_in:
+        with gzip.open(dest, 'wb') as file_out:
+            shutil.copyfileobj(file_in, file_out)
+
+
+logger = logging.getLogger(__name__)
+max_bytes = 50 * 1024 * 1024
+
+handler = logging.handlers.RotatingFileHandler('logs/request_logs.json', maxBytes=max_bytes, backupCount=10)
+handler.namer, handler.rotator = namer, rotator
 format_str = '%(msg)s %(asctime)s %(name)s %(levelname)s %(thread)s'
 formatter = jsonlogger.JsonFormatter(format_str)
 handler.setFormatter(formatter)
