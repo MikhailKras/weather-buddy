f8f5876089c15cb20120e1a247fb93c8d32baf5e
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-08-28 20:40:16 +0300

    fix: fixed multiple requests to endpoint with getting weather info by coordinates

--- src/static/js/weather_service/search.js
+++ src/static/js/weather_service/search.js
@@ -47,25 +47,40 @@ function searchByCoords(event) {
   errorMessage.style.display = "none";
   spinner.style.display = "inline-block";
 
-  redirect_url =
+  const redirect_url =
     "/weather/info/by_coordinates?latitude=" +
     encodeURIComponent(latitudeInput) +
     "&longitude=" +
     encodeURIComponent(longitudeInput);
-  fetch(redirect_url)
-    .then((response) => {
-      if (!response.ok) {
-        return response.json().then((data) => {
-          throw new Error(data.detail);
-        });
-      }
-      window.location.href = redirect_url;
-    })
-    .catch((error) => {
-      errorMessage.textContent = error.message;
-      errorMessage.style.display = "block";
-    })
-    .finally(() => {
-      spinner.style.display = "none";
-    });
+
+  // Use the History API to update the browser's URL
+  history.pushState(null, null, redirect_url);
+
+  // Check if there's an ongoing request
+  if (!searchByCoords.pendingRequest) {
+    searchByCoords.pendingRequest = true;
+
+    fetch(redirect_url)
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((data) => {
+            throw new Error(data.detail);
+          });
+        }
+        // Handle the redirection after the request is successful
+        return response.text();
+      })
+      .then((html) => {
+        // Render the HTML page from the response
+        document.body.innerHTML = html;
+      })
+      .catch((error) => {
+        errorMessage.textContent = error.message;
+        errorMessage.style.display = "block";
+      })
+      .finally(() => {
+        searchByCoords.pendingRequest = false;
+        spinner.style.display = "none";
+      });
+  }
 }
